70fbf93feb0b5bf02e429e204a2dda9f7a2865cb
Ralph Arsy Bauto <rab.devph@gmail.com>
2022-10-16 13:05:18 +0400

    Add user input for file extension

--- selective_copy.py
+++ selective_copy.py
@@ -14,15 +14,20 @@ from pathlib import Path
 check_path = Path.home() / 'selective_copy'
 destination_path = Path.home() / 'destination_folder'
 
-# Save file extensions in list.
-file_extensions = ['.jpg']
+# Add user input for file extensions.
+file_extension = input("File extensions/s(complete with dot[.] character), separated by space: ")
 
-# Loop through directory, sub-directory and files then
-# copy each file with the same extension in file_extensions list
-# to a new folder.
-for folder_name, sub_folders, file_names in os.walk(check_path):
-    for file_name in file_names:
-        file_name_extension = Path(file_name)
-        if file_name_extension.suffix in file_extensions:
-            shutil.copy(check_path / file_name, destination_path)
-            print(f"[{file_name}] COPIED TO {destination_path}.")
+# Save file extensions in list.
+file_extensions = file_extension.split()
+
+if file_extensions:
+
+    # Loop through directory, sub-directory and files then
+    # copy each file with the same extension in file_extensions list
+    # to a new folder.
+    for folder_name, sub_folders, file_names in os.walk(check_path):
+        for file_name in file_names:
+            file_name_extension = Path(file_name)
+            if file_name_extension.suffix in file_extensions:
+                shutil.copy(check_path / file_name, destination_path)
+                print(f"[{file_name}] COPIED TO {destination_path}.")
